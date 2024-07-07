df05eaf6b9d16008b79d8ebdc31068e67a599736
Quoc Thao <quocthao23061998.tg@gmail.com>
2024-07-07 12:07:30 +0700

    Initial Commit

---
+++ KPhucSinh/KPhucSinhApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPhucSinhApp.swift
+//  KPhucSinh
+//
+//  Created by Jin on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct KPhucSinhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
